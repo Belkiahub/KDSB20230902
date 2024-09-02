704ba2896ce96567b46085785b30717a35b76dcf
KevinSilva <kevinsilva462@gmail.com>
2024-09-02 17:13:23 -0600

    Agregar archivos de proyecto.

---
+++ KDSB20230902/Models/Alumno.cs
@@ -0,0 +1,11 @@
+﻿namespace KDSB20230902.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string grade { get; set; }
+
+    }
+}
